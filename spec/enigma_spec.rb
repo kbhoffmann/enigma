b43ad42f0141d1775af17d8861116638d8433d5a
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-11 11:32:48 -0700

    Test: create test to creating an instance of Enigma

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+SimpleCov.start
+require './lib/enigma'
+require 'pry'
+
+RSpec.describe Enigma do
+  it 'exists' do
+    enigma = Enigma.new
+
+    expect(enigma).to be_an_instance_of(Enigma)
+  end
+end
