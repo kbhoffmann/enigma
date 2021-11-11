032465f314c4d14163031ab6cf8b736d7da17935
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-11 13:02:20 -0700

    add pseudo code and notes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,21 @@
-class Enigma
+#require 'encrypt.rb'
+#require 'decrypt.rb'
 
-end 
+class Enigma
+  #message_file = File.open(ARGV[0], "r")
+end
+
+#to read a message to encrypt
+#message_data = message_file.read
+#message_file.close
+
+#to write to encrypt.rb
+#encrypted_message = File.open(ARGV[1], "w")
+#encrypted_message.write(method with final encryption returned)
+#encrypted_message.close
+
+# Useful Methods
+# Array#rotate
+# Date#strftime
+# Date::today
+# Enumerator#with_index”
