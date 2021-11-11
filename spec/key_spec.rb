0d80b03b7c39855f64dc2f94e74ded1c64c7515a
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-11 11:20:34 -0700

    Test: add test for generating a hash with letters A-D as keys and values from the separate sets method

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -23,4 +23,11 @@ RSpec.describe Key do
     expect(key.separate_sets).to be_an(Array)
     expect(key.separate_sets.length).to eq(4)
   end
+
+  it 'can create a hash of keys A thru D with sep. sets as values' do
+    key = Key.new
+
+    expect(key.create_keys_hash).to be_a(Hash)
+    expect(key.create_keys_hash.count).to eq(4)
+  end
 end
