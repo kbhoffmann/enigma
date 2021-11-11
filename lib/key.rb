9da573fd569c125e1aa2d148f81f7001422e1788
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-11 10:30:31 -0700

    Feat: add #separate_sets method

--- lib/key.rb
+++ lib/key.rb
@@ -4,4 +4,13 @@ class Key
     5.times.map {rand(10)}
   end
   #=> [0, 4, 0, 8, 1]
+
+  def separate_sets
+      keys = []
+    generate.each_cons(2) do |num|
+      keys << num
+    end
+    keys
+  end
+  #=>[[5, 8], [8, 4], [4, 6], [6, 1]]
 end
