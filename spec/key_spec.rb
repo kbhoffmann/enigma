1871361eb4dd28139e570c6d19fae82316820ea3
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-11 10:25:34 -0700

    Test: change variable key_1, to key

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -5,15 +5,21 @@ require 'pry'
 
 RSpec.describe Key do
   it 'exists' do
-    key_1 = Key.new
+    key = Key.new
 
-    expect(key_1).to be_an_instance_of(Key)
+    expect(key).to be_an_instance_of(Key)
   end
 
   it 'can generate a random 5 digit number' do
-    key_1 = Key.new
+    key = Key.new
 
-    expect(key_1.generate).to be_an(Array)
-    expect(key_1.generate.length).to eq(5)
+    expect(key.generate).to be_an(Array)
+    expect(key.generate.length).to eq(5)
+  end
+
+  xit 'can separate random number array into 4 sets of nums' do
+    key = Key.new
+
+    expect(key.generate).to
   end
 end
