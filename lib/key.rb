995ea9e27f8dc3d6fed05f5e3ef0286cf2c2a329
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-12 09:27:29 -0700

    add comment for last method to revise

--- lib/key.rb
+++ lib/key.rb
@@ -14,6 +14,12 @@ class Key
   end
   #=>[[5, 8], [8, 4], [4, 6], [6, 1]]
 
+
+#want 
+#A => 58,
+#B => 84,
+#c => 46
+#D => 61
   def create_keys_hash
     hash = Hash.new { |hash, key| hash[key] = [] }
     ('A'..'D').each do |letter|
