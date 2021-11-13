f1d557b4d1f72bcaf69864188d76f4c1ebe19913
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-13 10:00:31 -0700

    Feat: add helper method #check_character

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -16,10 +16,6 @@ class Encrypt
     message.downcase.split("")
   end
 
-  # def check_character(character)
-  #   character_set.include?(character)
-  # end
-
   def assign_shift_type
      shift_hash = {}
      message_array(@message).each_with_index do |letter, index|
@@ -40,22 +36,26 @@ class Encrypt
     new_letters
   end
 
+  def check_character(character)
+    @character_set.include?(character)
+  end
+
   def encrypted_letter
     translated_characters = {}
-    assign_shift_type.map do |type, letters|
+    assign_shift_type.map do |type, characters|
       #type = key of A,B,D,or D  #letters are values of ["e", " ", "l"], etc(4 sets)
       #values are the letters in an array grouped by shift type
-      letters.map do |letter|
+      characters.map do |character|
         #letter is each letter in one of the separate arrays
-        if @character_set.include?(letter)
+        if check_character(character)
           #first each letter is checked to see if its in the character set
           #if it is in the character set, the letter is translated according to
           #shift type and put into a new translated characters array.
-          (translated_characters[type] ||= []) << shifted_letters[type][letter]
+          (translated_characters[type] ||= []) << shifted_letters[type][character]
         else
           #if letter is not in the character set, put the character in the
           #set as it is(not translated).
-        (translated_characters[type] ||= []) << letter
+        (translated_characters[type] ||= []) << character
         end
       end
     end
