afbf49b66f4ce9baa1e5b224945b2ca10bd3c372
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-13 11:08:22 -0700

    Feat: add args for file i/o

--- encrypt.rb
+++ encrypt.rb
@@ -3,7 +3,21 @@
 #encrypted.txt is ARGV1
 # Created 'encrypted.txt' with the key 82648 and date 240818
 
-#message to encrypt => ARGV0
+#message file to encrypt => ARGV0
 #file to write encrypted message => ARGV1
+message_file = File.open(ARGV[0], "r")
+message_date = message_file.read
+message_file.close
+
+message = Enigma.new
+#???????encryption = encrypt.encrypted_message
+
+encryption_file = File.open(ARGV[1], "w")
+encryption_file.write(encryption)
+encryption_file.close
+
+#key and date path????? enigma class?
+#key =
+#date =
 
 puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
