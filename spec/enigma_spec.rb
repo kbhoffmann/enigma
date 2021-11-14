9720c65cd9e9097cf7625df329ee3942082bec9d
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-13 19:20:36 -0700

    Test: add test for #encrypt method with message, key, and date provided

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Enigma do
 
     expect(enigma).to be_an_instance_of(Enigma)
   end
+
+  it 'can encrypt a message with a given key and date' do
+    enigma = Enigma.new
+
+    encryption = {
+              :encryption => 'keder ohulw',
+              #the encrypted message
+              :key => '02715',
+              #the key used for encryption as a string
+              :date => '040895'
+              #the date used for encryption as a string
+                  }
+    expect(enigma.encrypt("hello world",'02715','40895')).to eq(encryption)
+  end
 end
