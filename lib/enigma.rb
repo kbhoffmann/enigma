5ce7b0a9738a39275b716726d20016338fb6b3cc
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-13 20:14:48 -0700

    Feat: add #Encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,15 @@
+require './lib/encrypt'
+require 'date'
+require 'pry'
+
 class Enigma
 
-end 
+  def encrypt(message, key = encrypt.key, date = encrypt.date)
+    encrypt = Encrypt.new(message, key, date)
+      {
+        :encryption => encrypt.encrypted_message,
+        :key =>  key,
+        :date => date
+      }
+  end
+end
