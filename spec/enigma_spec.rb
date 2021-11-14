004bb66a0c26ef1c754edd89ad8549cb12e2d299
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-13 21:05:38 -0700

    Test: add tests for encrypting message with optional date and key arguments

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -25,35 +25,36 @@ RSpec.describe Enigma do
     expect(enigma.encrypt('hello world','02715','040895')).to eq(encryption)
   end
 
-xit 'can encrypt a message with a given key and no date given' do
-  enigma = Enigma.new
+  it 'can encrypt a message with a given key and no date given' do
+    enigma = Enigma.new
 
-  encryption = {
-            :encryption => 'keder ohulw',
-            #the encrypted message
-            :key => '02715',
-            #the key used for encryption as a string
-            :date => 'todays date'
-            #the date used for encryption as a string
-  }
-  #date is an optional arg, if date is not included, today's date is used.
-  expect(encrypt.encrypt('hello world','02715',date =  nil)).to eq(encryption)
-end
+    encryption = {
+              :encryption => 'pkfawfqdzry',
+              #the encrypted message
+              :key => '02715',
+              #the key used for encryption as a string
+              :date => Date.today.strftime("%d%m%y")
+              #the date used for encryption as a string
+              }
+    #date is an optional arg, if date is not included, today's date is used.
+    expect(enigma.encrypt('hello world','02715')).to eq(encryption)
+  end
 
+#How do you test for randomness?
 xit 'can encrypt a message with NO key or date given' do
   enigma = Enigma.new
 
   encryption = {
-            :encryption => 'keder ohulw',
+            :encryption => String,
             #the encrypted message
-            :key => 'random key',
+            :key => String,
             #the key used for encryption as a string
-            :date => 'todays date'
+            :date => Date.today.strftime("%d%m%y")
             #the date used for encryption as a string
   }
   #key is optional arg, if key is not included, a random key is generated.
   #date is an optional arg, if date is not included, today's date is used.
-  expect(encrypt.encrypt('hello world')).to eq(encryption)
+  expect(enigma.encrypt('hello world')).to eq(encryption)
   end
 end
 
