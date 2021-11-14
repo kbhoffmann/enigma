08d1ac998db047d28c68f1ef3e3f937ba8416f8e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-14 06:58:33 -0700

    Test: add tests for decryption

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -45,9 +45,9 @@ xit 'can encrypt a message with NO key or date given' do
   enigma = Enigma.new
 
   encryption = {
-            :encryption => String,
+            :encryption => "Some String",
             #the encrypted message
-            :key => String,
+            :key => "Some key",
             #the key used for encryption as a string
             :date => Date.today.strftime("%d%m%y")
             #the date used for encryption as a string
@@ -56,49 +56,49 @@ xit 'can encrypt a message with NO key or date given' do
   #date is an optional arg, if date is not included, today's date is used.
   expect(enigma.encrypt('hello world')).to eq(encryption)
   end
-end
 
-# xit 'can decrypt a message with given key and date' do
-#   enigma = Enigma.new
-#
-#   expected = {
-#             :decryption => 'hello world',
-#             #the encrypted message
-#             :key => '02715',
-#             #the key used for encryption as a string
-#             :date => '040895'
-#             #the date used for encryption as a string
-#   }
-#   expect(enigma.decrypt(encrypted[:encryption], "02715")).to eq(expected)
-# end
-#
-# xit 'can decrypt a message with given key and no date given' do
-#   enigma = Enigma.new
-#
-#   expected = {
-#             :decryption => 'hello world',
-#             #the encrypted message
-#             :key => '02715',
-#             #the key used for encryption as a string
-#             :date => 'todays date'
-#             #the date used for encryption as a string
-#   }
-#   #date is an optional arg, if date is not included, today's date is used.
-#   expect(enigma.decrypt(encrypted[:encryption], "02715")).to eq(expected)
-# end
-#
-# xit 'can decrypt a message with NO date or time given' do
-#   enigma = Enigma.new
-#
-#   expected = {
-#             :decryption => 'hello world',
-#             #the encrypted message
-#             :key => 'random key',
-#             #the key used for encryption as a string
-#             :date => 'todays date'
-#             #the date used for encryption as a string
-#   }
-#   #key is optional arg, if key is not included, a random key is generated.
-#   #date is an optional arg, if date is not included, today's date is used.
-#   expect(enigma.decrypt(encrypted[:encryption])).to eq(expected)
-# end
+  xit 'can decrypt a message with given key and date' do
+    enigma = Enigma.new
+
+    expected = {
+              :decryption => 'hello world',
+              #the encrypted message
+              :key => '02715',
+              #the key used for encryption as a string
+              :date => '040895'
+              #the date used for encryption as a string
+    }
+    expect(enigma.decrypt(encrypted[:encryption], "02715")).to eq(expected)
+  end
+
+  xit 'can decrypt a message with given key and no date given' do
+    enigma = Enigma.new
+
+    expected = {
+              :decryption => 'hello world',
+              #the encrypted message
+              :key => '02715',
+              #the key used for encryption as a string
+              :date => 'todays date'
+              #the date used for encryption as a string
+    }
+    #date is an optional arg, if date is not included, today's date is used.
+    expect(enigma.decrypt(encrypted[:encryption], "02715")).to eq(expected)
+  end
+
+  xit 'can decrypt a message with NO date or time given' do
+    enigma = Enigma.new
+
+    expected = {
+              :decryption => 'hello world',
+              #the encrypted message
+              :key => 'random key',
+              #the key used for encryption as a string
+              :date => 'todays date'
+              #the date used for encryption as a string
+    }
+    #key is optional arg, if key is not included, a random key is generated.
+    #date is an optional arg, if date is not included, today's date is used.
+    expect(enigma.decrypt(encrypted[:encryption])).to eq(expected)
+  end
+end
