cb6f9ce658c39e19c54b34918c8c6c4216b0fdab
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-14 05:43:32 -0700

    update runner file

--- encrypt.rb
+++ encrypt.rb
@@ -1,23 +1,15 @@
-# $ ruby ./lib/encrypt.rb message.txt encrypted.txt
-#message.txt is ARGV0
-#encrypted.txt is ARGV1
-# Created 'encrypted.txt' with the key 82648 and date 240818
+require './lib/enigma'
 
-#message file to encrypt => ARGV0
-#file to write encrypted message => ARGV1
-message_file = File.open(ARGV[0], "r")
-message_date = message_file.read
-message_file.close
+message_file = File.open(ARGV[0], "r") #
+message = message_file.read #
+message_file.close #
+key = ARGV[2] #
+date = ARGV[3] #
+encrypting = Engima.new #
+encryption = encrypting.encrypt(message, key, date)
 
-message = Enigma.new
-#???????encryption = encrypt.encrypted_message
+encrypt_file = File.open(ARGV[1], "w") #
+encrypt_file.write(encryption[:encryption])
+encrypt_file.close
 
-encryption_file = File.open(ARGV[1], "w")
-encryption_file.write(encryption)
-encryption_file.close
-
-#key and date path????? enigma class?
-#key =
-#date =
-
-puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
+puts "Created '#{ARGV[1]}' with the key #{encryption[:key]} and date #{encryption[:date]}"
