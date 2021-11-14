585325971289d4dff1f09408a7f60ca840f5035f
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-14 07:22:17 -0700

    Feat: initialize with instance variables and corresponding attr_readers

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,2 +1,15 @@
+require 'date'
+require_relative '../modules/shiftable'
+
 class Decrypt
-end 
+  include Shiftable
+  attr_reader :message, :key, :date, :character_set, :shift_types
+  def initialize(message, key = self.generate, date = self.get_date)
+    @message = message
+    @key = key
+    @date = date.rjust(6, "0")
+    @character_set = ("a".."z").to_a << " "
+    @shift_types = ["A", "B", "C", "D"]
+  end
+
+end
