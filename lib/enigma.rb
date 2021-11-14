0bdc2b21cb32d9075e89caa7f6b85e09a68caaa8
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-14 10:26:48 -0700

    Feat: remove unneeded instance variables and attr_readers

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,18 +1,11 @@
 require './lib/encrypt'
+require './lib/decrypt'
 require_relative '../modules/shiftable'
 require 'date'
 require 'pry'
 
 class Enigma
   include Shiftable
-
-  # attr_reader #:key, :date #:message, :cipher_text,
-  def initialize
-    # @message = message
-    # @cipher_text = cipher_text
-    # @key = key
-    # @date = date
-  end
 
   def encrypt(message, key, date)
     if key.nil?
