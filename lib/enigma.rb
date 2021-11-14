bb335acfe35668f2c8414bb835968d75ce9a849f
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-13 21:06:40 -0700

    Feat: add attr_readers and instance variables message, cipher_text, key, and date to initialize

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,24 @@
 require './lib/encrypt'
+require_relative '../modules/shiftable'
 require 'date'
 require 'pry'
 
 class Enigma
+  include Shiftable
 
-  def encrypt(message, key = encrypt.key, date = encrypt.date)
+  attr_reader :message, :cipher_text, :key, :date
+  def initialize
+    @message = message
+    @cipher_text = cipher_text
+    @key = key
+    @date = date
+  end
+
+  def encrypt(message, key = self.generate, date = self.get_date)
     encrypt = Encrypt.new(message, key, date)
       {
         :encryption => encrypt.encrypted_message,
-        :key =>  key,
+        :key => key,
         :date => date
       }
   end
