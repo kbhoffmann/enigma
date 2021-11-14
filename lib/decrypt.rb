ce7990040217295690dbfdb9e6c3db3e286d8ccd
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-14 07:26:19 -0700

    Feat: add assign_shift_type

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -15,5 +15,13 @@ class Decrypt
   def message_array(message)
     message.downcase.split("")
   end
-  
+
+  def assign_shift_type
+     shift_hash = {}
+     message_array(@message).each_with_index do |letter, index|
+       type = index % @shift_types.count
+        (shift_hash[@shift_types[type]] ||= []) << letter
+    end
+    shift_hash
+  end
 end
