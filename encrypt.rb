1006967e2f4416867c1730eeb023555ccfee513c
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-15 21:53:13 -0700

    remove key and date arguments

--- encrypt.rb
+++ encrypt.rb
@@ -1,14 +1,13 @@
 require './lib/enigma'
 
-message_file = File.open(ARGV[0], "r") #
-message = message_file.read #
-message_file.close #
-key = ARGV[2] #
-date = ARGV[3] #
-encrypting = Enigma.new #
-encryption = encrypting.encrypt(message, key, date)
+message_file = File.open(ARGV[0], "r")
+message = message_file.read
+message_file.close
 
-encrypt_file = File.open(ARGV[1], "w") #
+encrypting = Enigma.new
+encryption = encrypting.encrypt(message)
+
+encrypt_file = File.open(ARGV[1], "w")
 encrypt_file.write(encryption[:encryption])
 encrypt_file.close
 
