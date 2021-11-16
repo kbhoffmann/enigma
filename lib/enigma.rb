82088b1e4019c11a429f24787a9540a972fc25dc
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-15 21:59:26 -0700

    edit encrypt and decrypt to create optional arguments and add instance variables to both

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,32 +5,41 @@ require 'date'
 require 'pry'
 
 class Enigma
-  include Shiftable
+include Shiftable
+attr_reader :message,
+      :cipher_text,
+      :key,
+      :date
 
-  def encrypt(message, key, date)
-    if key.nil?
-      key = self.generate
-    end
-    if date.nil?
-      date = self.get_date
-    end
-    encrypt = Encrypt.new(message, key, date)
-      {
-        :encryption => encrypt.encrypted_message,
-        :key => key,
-        :date => date
-      }
-  end
+def initialize
+  @message     = message
+  @cipher_text = cipher_text
+  @key         = key
+  @date        = date
+end
 
-  def decrypt(ciphertext, key, date)
-    if date.nil?
-      date = self.get_date
-    end
-    decrypt = Decrypt.new(ciphertext, key, date)
-      {
-        :decryption => decrypt.decrypted_message,
-        :key => key,
-        :date => date
-      }
+def encrypt(message, key = self.generate, date = self.get_date)
+  @message  = message
+  @key      = key
+  @date     = date
+  encrypt = Encrypt.new(@message, @key, @date)
+    {
+      :encryption => encrypt.encrypted_message,
+      :key => key,
+      :date => date
+    }
+end
+
+def decrypt(ciphertext, key, date = self.get_date)
+  @ciphertext  = ciphertext
+  @key      = key
+  @date     = date
+  decrypt = Decrypt.new(@ciphertext, @key, @date)
+
+    {
+      :decryption => decrypt.decrypted_message,
+      :key => key,
+      :date => date
+    }
   end
 end
