c91de37edd45e28875d46a98b6e3dfc74e10f7d1
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-15 22:10:19 -0700

    Test: remove unneeded tests and remove comments

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,27 +10,13 @@ RSpec.describe Enigma do
     expect(enigma).to be_an_instance_of(Enigma)
   end
 
-  it 'can encrypt a message with a given key and date' do
+  it 'can encrypt a message with no key and no date given' do
     enigma = Enigma.new
 
-    encrypted = {
-              :encryption => 'keder ohulw',
-              :key => '02715',
-              :date => '040895'
-               }
-    expect(enigma.encrypt('hello world','02715','040895')).to eq(encrypted)
-  end
-
-  it 'can encrypt a message with a given key and no date given' do
-    enigma = Enigma.new
-
-    encrypted = {
-              :encryption => 'pkfawfqdzry',
-              :key => '02715',
-              :date => Date.today.strftime("%d%m%y")
-              }
-
-    expect(enigma.encrypt('hello world','02715', date = nil)).to eq(encrypted)
+    expect(enigma.encrypt('hello world')[:encryption]).to be_a(String)
+    expect(enigma.encrypt('hello world')[:key].to_i).to be_an(Integer)
+    expect(enigma.encrypt('hello world')[:key].length).to eq(5)
+    expect(enigma.encrypt('hello world')[:date]).to eq(Date.today.strftime("%d%m%y"))
   end
 
   it 'can decrypt a message with given key and date' do
@@ -45,7 +31,7 @@ RSpec.describe Enigma do
     expect(enigma.decrypt('keder ohulw','02715','040895')).to eq(decryption)
   end
 
-  xit 'can use todays date to decrypt if key is given without date' do
+  it 'can use todays date to decrypt if key is given without date' do
     enigma = Enigma.new
 
     decryption = {
@@ -53,26 +39,7 @@ RSpec.describe Enigma do
               :key => '02715',
               :date => Date.today.strftime("%d%m%y")
             }
-
-    expect(enigma.decrypt(encrypted[:encryption], date = nil)).to eq(decryption)
+    encrypted = enigma.encrypt("hello world", "02715")
+    expect(enigma.decrypt(encrypted[:encryption], "02715")).to eq(decryption)
   end
 end
-
-
-
-#How do you test for randomness?
-# xit 'can encrypt a message with NO key or date given' do
-#   enigma = Enigma.new
-#
-#   encryption = {
-#             :encryption => "Some String",
-#             #the encrypted message
-#             :key => "Some key",
-#             #the key used for encryption as a string
-#             :date => Date.today.strftime("%d%m%y")
-#             #the date used for encryption as a string
-#   }
-#   #key is optional arg, if key is not included, a random key is generated.
-#   #date is an optional arg, if date is not included, today's date is used.
-#   expect(enigma.encrypt('hello world')).to eq(encryption)
-#   end
